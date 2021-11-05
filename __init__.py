f03b41a7d002825c7ba37c3bd87b1df0c94f171d
ECEN361JarvisTeam <92492047+ECEN361JarvisTeam@users.noreply.github.com>
2021-11-05 12:19:34 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,25 @@
 from mycroft import MycroftSkill, intent_file_handler
+import time
+import maestro
 
 
 class NodAndShake(MycroftSkill):
+    m = maestro.Controller("/dev/ttyACM0")
+    
     def __init__(self):
         MycroftSkill.__init__(self)
 
     @intent_file_handler('shake.and.nod.intent')
     def handle_shake_and_nod(self, message):
-        self.speak_dialog('shake.and.nod')
+        #self.speak_dialog('shake.and.nod')
+        m.setAccel(1,1)
+        m.setSpeed(1,4)
+        m.setTarget(1,5000)
+        time.sleep(3000)
+        m.setTarget(1,4000)
+        m.close()
 
+        
 
 def create_skill():
     return NodAndShake()
