c7f50536599ce802982433bc2021b74b4bc67917
ECEN361JarvisTeam <noahkfish@gmail.com>
2021-11-10 17:20:39 -0700

    Made a proper nod.

--- __init__.py
+++ __init__.py
@@ -1,12 +1,35 @@
 from mycroft import MycroftSkill, intent_file_handler
+import time
+import maestro
 
 
 class NodAndShake(MycroftSkill):
+    
     def __init__(self):
         MycroftSkill.__init__(self)
 
     @intent_file_handler('shake.and.nod.intent')
     def handle_shake_and_nod(self, message):
+        m = maestro.Controller("/dev/ttyACM0")
+        m.setAccel(1, 4)
+        m.setSpeed(1, 12)
+        # 8000 seems to be the maximum
+        m.setTarget(1, 8000)
+        while m.getPosition(1) < 8000:
+            time.sleep(0.01)
+        
+        m.setTarget(1, 5500)
+
+        while m.getPosition(1) > 5500:
+            time.sleep(0.01)
+
+        m.setTarget(1, 7000)
+
+        while m.getPosition(1) < 7000:
+            print(m.getPosition(1))
+            time.sleep(0.01)
+        
+        m.close()
         self.speak_dialog('shake.and.nod')
 
 
