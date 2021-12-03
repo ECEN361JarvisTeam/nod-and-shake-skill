b26997fd391da4d81ac67187fee96a454b6af432
ECEN361JarvisTeam <noahkfish@gmail.com>
2021-12-03 15:37:11 -0700

    Added 'shake' and 'nod and shake' commands.

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from mycroft import MycroftSkill, intent_file_handler
 import time
 import maestro
+import maestro_functions as mf
 
 
 class NodAndShake(MycroftSkill):
@@ -11,27 +12,64 @@ class NodAndShake(MycroftSkill):
 
     @intent_file_handler('shake.and.nod.intent')
     def handle_shake_and_nod(self, message):
+        self.speak_dialog('shake.and.nod', {"action": message.data["utterance"]})
+        time.sleep(0.5)
+
         m = maestro.Controller("/dev/ttyACM0")
-        m.setAccel(1, 4)
-        m.setSpeed(1, 12)
-        # 8000 seems to be the maximum
-        m.setTarget(1, 8000)
-        while m.getPosition(1) < 8000:
-            time.sleep(0.01)
+        mf.setAllToDefault(m)
+        time.sleep(1)
+
+        if message.data["utterance"] == "nod":
+            # 8000 seems to be the maximum
+            m.setTarget(mf.h_v, 8000)
+            while m.getPosition(mf.h_v) < 8000:
+                time.sleep(0.01)
         
-        m.setTarget(1, 5500)
+            m.setTarget(mf.h_v, 5500)
 
-        while m.getPosition(1) > 5500:
-            time.sleep(0.01)
+            while m.getPosition(mf.h_v) > 5500:
+                time.sleep(0.01)
 
-        m.setTarget(1, 7000)
+            m.setTarget(mf.h_v, mf.h_vDefault)
 
-        while m.getPosition(1) < 7000:
-            print(m.getPosition(1))
-            time.sleep(0.01)
+            while m.getPosition(mf.h_v) < mf.h_vDefault:
+                time.sleep(0.01)
+
+            m.close()
+
+        elif message.data["utterance"] == "shake":
+            # 8000 seems to be the maximum
+            m.setTarget(mf.h_r, 8000)
+            while m.getPosition(mf.h_r) < 8000:
+                time.sleep(0.01)
         
-        m.close()
-        self.speak_dialog('shake.and.nod')
+            m.setTarget(mf.h_r, 4000)
+
+            while m.getPosition(mf.h_r) > 4000:
+                time.sleep(0.01)
+
+            m.setTarget(mf.h_r, mf.h_rDefault)
+
+            while m.getPosition(mf.h_r) < mf.h_rDefault:
+                time.sleep(0.01)
+
+            m.close()
+
+        else:
+            # Nod and Shake
+            mf.setPositionThread(m, mf.h_v, 8000, 12, 4)
+            time.sleep(0.01)
+            mf.setPositionThread(m, mf.h_v, 5500, 12, 4)
+            time.sleep(0.01)
+            mf.setPositionThread(m, mf.h_v, mf.h_vDefault, 12, 4)
+            time.sleep(0.01)
+            mf.setPositionThread(m, mf.h_r, 8000, 12, 4)
+            time.sleep(0.01)
+            mf.setPositionThread(m, mf.h_r, 4000, 12, 4)
+            time.sleep(0.1)
+            mf.setPositionThread(m, mf.h_r, mf.h_rDefault, 12, 4)
+            time.sleep(3)
+            mf.closeThreadedMaestro(m)
 
         
 
